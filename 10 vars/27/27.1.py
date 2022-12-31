f1cd31c1c562381e7cd9464ed34a3a0ab8265fcd
Kate Petrova <2005kpetrova@gmail.com>
2022-12-31 16:26:32 +0300

    1 var restored

--- 10 vars/27/27.1.py
+++ 10 vars/27/27.1.py
@@ -1,4 +1,4 @@
-with open('27b.txt') as f:
+with open('27-B.txt') as f:
     n = int(f.readline())
     min_0, min_1 = 1001, 1001
     k_0, k_1 = 0, 0
@@ -8,12 +8,18 @@ with open('27b.txt') as f:
         x = int(i)
         if x % 2:
             k_1 += 1
-            min_1 = min(min_1, x)
+            if min_1 == 1001:
+                min_1 = x
+            else:
+                min_1 = min(min_1, x)
             if x > min_0:
                 k += k_0
         else:
             k_0 += 1
-            min_0 = min(min_0, x)
+            if min_0 == 1001:
+                min_0 = x
+            else:
+                min_0 = min(min_0, x)
             if x > min_1:
                 k += k_1
 print(k)
